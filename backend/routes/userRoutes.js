4555c22c7a14831826c4dc9aef1834db70cf8379
Hieu4short <hieu.haminh2255@gmail.com>
2025-09-07 10:49:58 +0700

    Update user feature

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser, getAllUsers } from "../controllers/userController.js";
+import { createUser, 
+    loginUser, 
+    logoutCurrentUser, 
+    getAllUsers,
+    getCurrentUserProfile,
+    updateCurrentUserProfile,
+    deleteUserById,
+    getUserById,
+    updateUserById } from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -7,5 +15,10 @@ const router = express.Router();
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth', loginUser);
 router.post('/logout', logoutCurrentUser);
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
+//Admin routes
+router.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById)
+.get(authenticate, authorizeAdmin, getUserById)
+.put(authenticate, authorizeAdmin, updateUserById);
 export default router;
